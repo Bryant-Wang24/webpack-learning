a09f44a67ef12ef6a16f9a9a047c2ccad50af9e2
王秋爽 <wangqiushuang@liankenet.com>
2021-11-09 16:00:45 +0800

    使用HtmlWebpackPlugin自动打包更新html

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode:'production',
     entry:{
         app:'./src/index.js',
         print:'./src/print.js'
     },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title:'管理输出'
+        })
+    ],
     output:{
         filename:'[name].main.js',
         path:path.resolve(__dirname,'dist')
