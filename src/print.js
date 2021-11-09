9bd94d608df88cc9b3ecd34af90858570fb07438
王秋爽 <wangqiushuang@liankenet.com>
2021-11-09 17:24:13 +0800

    模块热替换 启用 HMR

--- src/print.js
+++ src/print.js
@@ -1,4 +1,6 @@
 export default function printMe() {
     console.log('I get called from print.js!');
     console.log('fuck');
+    console.log('heihei');
+    console.log('Updating print.js...')
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,28 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     mode: 'development',
     entry:{
-        app:'./src/index.js',
-        print:'./src/print.js'
+        app: './src/index.js'
     },
     // JavaScript 提供了 source map 功能，可以将编译后的代码映射回原始源代码。如果一个错误来自于 b.js，source map 就会明确的告诉你。
     devtool: 'inline-source-map',//source-map详细内容： 'https://v4.webpack.docschina.org/configuration/devtool'
 
 //    告知 webpack-dev-server，将 dist 目录下的文件 serve 到 localhost:8080 下。（译注：serve，将资源作为 server 的可访问文件）
-    devServe:{
-        contentBase:'./dist'
+    devServer:{
+        hot: true
+        // contentBase: './dist'
     },
 
     plugins:[
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title:'开发环境'
-        })
+            title:'模块热替换'
+        }),
+        new webpack.HotModuleReplacementPlugin()
     ],
     output:{
         filename:'[name].main.js',
