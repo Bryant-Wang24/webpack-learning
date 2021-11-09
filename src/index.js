bb55a225e272e8918f3f17758e5f1dab59e19a96
王秋爽 <wangqiushuang@liankenet.com>
2021-11-09 15:22:49 +0800

    使用file-loader加载image图像

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.png';
 function component() {
     let element = document.createElement('div');
   
-    // lodash（目前通过一个 script 引入）对于执行这一行是必需的
+    // lodash，现在由此脚本导入
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    // 将图像添加到已经存在的div中
+    var myIcon = new Image()
+    myIcon.src = Icon
+    element.appendChild(myIcon)
   
     return element;
   }
