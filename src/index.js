6c6238c4399cc1f695b19566ce10c3a61a33bec8
王秋爽 <wangqiushuang@liankenet.com>
2021-11-09 18:06:25 +0800

    访问接口的第二种写法:module.hot

--- src/index.js
+++ src/index.js
@@ -16,10 +16,18 @@ function component() {
   
   document.body.appendChild(component());
 // console.log('哈哈',import.meta.webpackHot.accept());
-  if (import.meta.webpackHot) {
-    console.log('执行');
-    import.meta.webpackHot.accept('./print.js', function() {
+// 第一种写法
+  // if (import.meta.webpackHot) {
+  //   console.log('执行');
+  //   import.meta.webpackHot.accept('./print.js', function() {
+  //        console.log('更新后触发的函数');
+  //        printMe();
+  //      })
+  // }
+  // 第二种写法
+  if (module.hot) {
+    module.hot.accept('./print.js', function() {
          console.log('更新后触发的函数');
          printMe();
        })
-     }
+  }
--- src/print.js
+++ src/print.js
@@ -1,4 +1,5 @@
 export default function printMe() {
     console.log('Updating print.js...')
     console.log('fuck');
+    console.log('更新');
   }
